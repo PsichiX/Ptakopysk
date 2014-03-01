aae5bf7b9eccc1f174dcf64a17c4f5136440ef4f
PsichiX <psichix@psichix.com>
2014-03-01 21:26:26 +0100

    sdk binaries moved to bin folder

--- makesdk.sh
+++ makesdk.sh
@@ -14,10 +14,14 @@ cp -r ./development/requirements/xenon-core-3-sdk/Code/Engine/XenonCore3/include
 # lib
 mkdir -p ./sdk/lib/
 cp ./development/Ptakopysk/bin/*.a ./sdk/lib/
-cp ./development/Ptakopysk/bin/*.dll ./sdk/lib/
 cp ./development/requirements/Box2D/bin/Debug/libBox2D.a ./sdk/lib/libBox2D-d.a
 cp ./development/requirements/Box2D/bin/Release/libBox2D.a ./sdk/lib/libBox2D.a
 cp ./development/requirements/JsonCpp/bin/Debug/libJsonCpp.a ./sdk/lib/libJsonCpp-d.a
 cp ./development/requirements/JsonCpp/bin/Release/libJsonCpp.a ./sdk/lib/libJsonCpp.a
 cp ./development/requirements/SFML-2.1/lib/*.a ./sdk/lib/
 cp ./development/requirements/xenon-core-3-sdk/Code/Engine/XenonCore3/libs/*.a ./sdk/lib/
+
+# bin
+mkdir -p ./sdk/bin/
+cp ./development/Ptakopysk/bin/*.dll ./sdk/bin/
+cp ./development/requirements/SFML-2.1/bin/*.dll ./sdk/bin/
